0118b83924cc5c1282ffed6cf29242d2dd9d3b81
Lucian Cioba <luciancioba@gmail.com>
2024-04-04 17:35:56 +0300

    update main and learning slice lists & tuples

--- intermediate/28-snake/main.py
+++ intermediate/28-snake/main.py
@@ -26,10 +26,28 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-# Detect collision:
+    # Detect collision:
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
+    """ Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over() """
 
+    # Detect collision with tail (short code with slice)
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
+
