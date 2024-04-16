ad48b0a62dd088f8b75945332f075da1a908a0a2
Lucian Cioba <luciancioba@gmail.com>
2024-04-16 15:05:24 +0300

    update main for usa-game

--- intermediate/33-usa-game/main.py
+++ intermediate/33-usa-game/main.py
@@ -19,14 +19,14 @@ while len(guessed_states) < 50:
         for state in all_states:
             if state not in guessed_states:
                 missing_states.append(state)
-            new_data = pandas.DataFrame(missing_states)
-            new_data.to_csv("states_to_learn.csv")
-            break
-        if answer_state in all_states:
-            guessed_states.append(answer_state)
-            t = turtle.Turtle()
-            t.hideturtle()
-            t.penup()
-            state_data = data[data.state == answer_state]
-            t.goto(int(state_data.x), int(state_data.y))
-            t.write(answer_state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
