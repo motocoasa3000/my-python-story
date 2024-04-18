70c430a97f6d5759a79d6ad2b063b3fc25c193f9
Lucian Cioba <luciancioba@gmail.com>
2024-04-18 17:42:58 +0300

    Using a **kwargs dictionary safely

--- intermediate/35-many-keyword-arguments.py
+++ intermediate/35-many-keyword-arguments.py
@@ -6,6 +6,8 @@ def add(*args):
     for n in args:
         sum += n
     return sum
+
+
 # print(add(3, 5, 6, 2, 1, 7, 4, 3))
 
 
@@ -21,3 +23,17 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=3, multiply=5)
+
+
+# How to use a **kwargs dictionary safely
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
