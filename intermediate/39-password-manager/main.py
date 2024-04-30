f556e408fdfdf0fa59ee3d0f4a73a2360c6db324
Lucian Cioba <luciancioba@gmail.com>
2024-04-30 17:30:37 +0300

    update main for password manager

--- intermediate/39-password-manager/main.py
+++ intermediate/39-password-manager/main.py
@@ -25,10 +25,17 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
+email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add")
+add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
