f05294969d8940cf97727f076ed6ba0c8160be5a
Lucian Cioba <luciancioba@gmail.com>
2024-05-04 14:48:21 +0300

    update Dialog boxes and pop-ups in tkinter for password manager

--- intermediate/39-password-manager/main.py
+++ intermediate/39-password-manager/main.py
@@ -11,13 +11,16 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                          f"\nPassword: {password} \nDo you want to save the password?")
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="The fields are empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                              f"\nPassword: {password} \nDo you want to save the password?")
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
