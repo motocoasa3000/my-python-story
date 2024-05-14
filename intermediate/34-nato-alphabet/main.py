2e659e7a7f5f043afd8cdae943d050d663141342
Lucian Cioba <luciancioba@gmail.com>
2024-05-14 18:42:47 +0300

    except handling in nato alphabet update

--- intermediate/34-nato-alphabet/main.py
+++ intermediate/34-nato-alphabet/main.py
@@ -5,6 +5,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def nato_word():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter letters please.")
+        nato_word()  # call the function here to loop if you enter number/symbol etc
+    else:
+        print(output_list)
+
+
+nato_word()
