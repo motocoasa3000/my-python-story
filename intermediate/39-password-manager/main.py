e12e8fc689a67372b3a983097aeb2d50525e9c9d
Lucian Cioba <luciancioba@gmail.com>
2024-05-17 22:01:29 +0300

    update main file for password manager

--- intermediate/39-password-manager/main.py
+++ intermediate/39-password-manager/main.py
@@ -41,16 +41,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="The fields are empty!")
     else:
-        with open("data.json", "r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Update old data with new data
             data.update(new_data)
 
-        with open("data.json", "w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
