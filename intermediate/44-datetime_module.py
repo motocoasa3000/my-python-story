49831692df9b2dfd8202aa65376bc6057dbd01c8
Lucian Cioba <luciancioba@gmail.com>
2024-05-25 17:35:33 +0300

    datetime module course

---
+++ intermediate/44-datetime_module.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1994, month=10, day=31 , hour=3)
+print(date_of_birth)
