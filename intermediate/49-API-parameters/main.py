50fd3889fe1686f2d05b4acb58f2e7a1091da11c
Lucian Cioba <luciancioba@gmail.com>
2024-06-04 17:16:21 +0300

    understand API parameters

---
+++ intermediate/49-API-parameters/main.py
@@ -0,0 +1,14 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 46.744579
+MY_LNG = 23.484989
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+print(sunrise)
+print(sunset)
