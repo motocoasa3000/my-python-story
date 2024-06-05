440e1d6e13e31b752d582aeaca91a5da2f9cf143
Lucian Cioba <luciancioba@gmail.com>
2024-06-05 17:35:14 +0300

    iss above floresti notifier

---
+++ intermediate/50-iss-overhead-notifier/main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "lucianflorincioba@gmail.com"
+MY_PASSWORD = "vfdp ocod zdck sceo"
+MY_LAT = 46.744579
+MY_LNG = 23.484989
+
+
+def is_iss_overhead():
+    response = requests.get(url="https://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # MY_LAT and MY_LNG position +5/-5 of the iss position
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LNG - 5 <= iss_longitude <= MY_LNG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Don't look up\n\nThe ISS is above Floresti City"
+        )
