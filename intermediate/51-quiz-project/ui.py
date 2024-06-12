4efb0b3f6ea261d5dbe9b46daa73d047bce209f7
Lucian Cioba <luciancioba@gmail.com>
2024-06-12 17:58:12 +0300

    update main and user interface for quiz project

--- intermediate/51-quiz-project/ui.py
+++ intermediate/51-quiz-project/ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizz App")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -15,7 +17,8 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(
-            150, 125, text="Some Question Text", fill=THEME_COLOR,
+            150, 125, width=250,
+            text="Some Question Text", fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
@@ -28,4 +31,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=0)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
