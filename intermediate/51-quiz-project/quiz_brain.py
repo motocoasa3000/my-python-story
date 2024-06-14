6135d8b17e0f2b38961bac50bed4f5f0636c4f7d
Lucian Cioba <luciancioba@gmail.com>
2024-06-14 13:45:22 +0300

    quiz brain and ui files update

--- intermediate/51-quiz-project/quiz_brain.py
+++ intermediate/51-quiz-project/quiz_brain.py
@@ -12,19 +12,20 @@ class QuizBrain:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(current_question.text)
+        q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         # self.check_answer(user_answer, current_question.answer)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
             print("You got it right!")
         else:
             print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}.")
+
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
