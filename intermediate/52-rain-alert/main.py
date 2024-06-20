776ecfdb7197ad0371d21f0698471d770e2104f5
Lucian Cioba <luciancioba@gmail.com>
2024-06-20 16:38:06 +0300

    Check if it will rain

--- intermediate/52-rain-alert/main.py
+++ intermediate/52-rain-alert/main.py
@@ -7,7 +7,18 @@ weather_params = {
     "lat": 46.744579,
     "lon": 23.484989,
     "appid": api_key,
+    "cnt": 4,
 }
 
 response = requests.get(owm_endpoint, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
