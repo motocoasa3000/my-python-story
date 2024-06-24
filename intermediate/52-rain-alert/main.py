917cd528dfef8772fbe73753f88da9216a59ad46
Lucian Cioba <luciancioba@gmail.com>
2024-06-24 18:36:38 +0300

    sending sms via trilio

--- intermediate/52-rain-alert/main.py
+++ intermediate/52-rain-alert/main.py
@@ -3,12 +3,12 @@ from twilio.rest import Client
 
 owm_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "0093a96b010af35e9a47882d48721ab4"
-account_sid = 'AC71a682495ba7f2fe8c9974bcfd048c7b'
-auth_token = '[AuthToken]'
+account_sid = "AC71a682495ba7f2fe8c9974bcfd048c7b"
+auth_token = "53840fb8770c0b8ae8f2a165c1e4b8bc"
 
 weather_params = {
-    "lat": 46.744579,
-    "lon": 23.484989,
+    "lat": 45.073605,
+    "lon": 25.434038,
     "appid": api_key,
     "cnt": 4,
 }
@@ -24,4 +24,12 @@ for hour_data in weather_data["list"]:
     if int(condition_code) < 700:
         will_rain = True
 if will_rain:
-    print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='+16207431718',
+        to='yournr',
+        body="Hello"
+    )
+
+    print(message.sid)
