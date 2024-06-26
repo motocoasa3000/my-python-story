f76f01ca729c30b106dacf5fd96a111f8abd4106
Lucian Cioba <luciancioba@gmail.com>
2024-06-26 15:38:52 +0300

    stock news project

--- intermediate/53-stock-news-project/main.py
+++ intermediate/53-stock-news-project/main.py
@@ -9,8 +9,9 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = "7ZOMIU2N834EY0LS"
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-""" 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+
+# 1- Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g.
+# [new_value for (key, value) in dictionary.items()]
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
@@ -18,7 +19,16 @@ stock_params = {
 }
 
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-#TODO 2. - Get the day before yesterday's closing stock price
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(data_list)
+
+# 2-Get the day before yesterday's closing stock price
+day_before_yesterday = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday["4. close"]
+print(day_before_yesterday_closing_price)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 
@@ -44,7 +54,7 @@ response = requests.get(STOCK_ENDPOINT, params=stock_params)
 
 
 # Optional TODO: Format the message like this:
-
+"""
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
