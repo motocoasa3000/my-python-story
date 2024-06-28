f7cb015681301c08c8edfbfad6042f8e680bffa9
Lucian Cioba <luciancioba@gmail.com>
2024-06-28 16:09:09 +0300

    update stock news project - get the news articles

--- intermediate/53-stock-news-project/main.py
+++ intermediate/53-stock-news-project/main.py
@@ -7,6 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "7ZOMIU2N834EY0LS"
+NEWS_API = "9d4a63a4ce5f48caa6a3e932c80c0162"
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
@@ -32,7 +33,7 @@ print(day_before_yesterday_closing_price)
 
 # 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint:
 # https://www.w3schools.com/python/ref_func_abs.asp
-difference = abs(float(yesterday_closing_price - day_before_yesterday_closing_price))
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
 # 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before
@@ -40,16 +41,23 @@ print(difference)
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 4:
-    print("Get News")
+# 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if diff_percent > 1:
+    news_params = {
+        "apiKey": NEWS_API,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
     # STEP 2: https://newsapi.org/
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+    three_articles = articles[:3]
+    print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number.
