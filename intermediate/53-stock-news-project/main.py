817e8818358d3f744c6befef7c8ade5d58e7c55b
Lucian Cioba <luciancioba@gmail.com>
2024-06-29 15:04:29 +0300

    finish..ish.. stock news project

--- intermediate/53-stock-news-project/main.py
+++ intermediate/53-stock-news-project/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +9,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "7ZOMIU2N834EY0LS"
 NEWS_API = "9d4a63a4ce5f48caa6a3e932c80c0162"
+TWILIO_SID = "AC71a682495ba7f2fe8c9974bcfd048c7b"
+TWILIO_AUTH_TOKEN = ""
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
@@ -33,16 +36,21 @@ print(day_before_yesterday_closing_price)
 
 # 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint:
 # https://www.w3schools.com/python/ref_func_abs.asp
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 print(difference)
 
 # 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before
 # yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
 # 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 1:
+if abs(diff_percent) > 1:
     news_params = {
         "apiKey": NEWS_API,
         "qInTitle": COMPANY_NAME,
@@ -53,20 +61,31 @@ if diff_percent > 1:
     # STEP 2: https://newsapi.org/
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    # 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    # 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint:
+    # https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
     print(three_articles)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number.
+    # STEP 3: Use twilio.com/docs/sms/quickstart/python
+    # to send a separate message with each article's title and description to your phone number.
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    # 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
-#TODO 9. - Send each article as a separate message via Twilio.
+    formatted_articles = [f"{STOCK_NAME}Headline: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in
+                          three_articles]
 
+    # 9. - Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
+    for article in formatted_articles:
+        message = client.messages.create(
+            from_='+16207431718',
+            to='yournr'
+        )
+
+    print(message.sid)
 
 # Optional TODO: Format the message like this:
 """
@@ -78,4 +97,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
