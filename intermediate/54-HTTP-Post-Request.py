668c33c804829310a95ccb838ae6376379afd268
Lucian Cioba <luciancioba@gmail.com>
2024-07-01 16:45:02 +0300

    Authentication using HTTP header

--- intermediate/54-HTTP-Post-Request.py
+++ intermediate/54-HTTP-Post-Request.py
@@ -1,13 +1,29 @@
 import requests
 
+USERNAME = "luci"
+TOKEN = "asdfghjkl"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "asdfghjkl",
-    "username": "luci",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config)
 print(response.text)
