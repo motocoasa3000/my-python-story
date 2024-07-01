49eb3cfbd2dbf3ac31231569d6b8d587c270bd0f
Lucian Cioba <luciancioba@gmail.com>
2024-07-01 17:22:59 +0300

    Authentication using HTTP header

--- intermediate/54-HTTP-Post-Request.py
+++ intermediate/54-HTTP-Post-Request.py
@@ -25,5 +25,9 @@ graph_config = {
     "color": "sora"
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
