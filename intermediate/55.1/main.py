1463c8d3b00d55e05ff71ffc05e7307d6ca07427
Lucian Cioba <luciancioba@gmail.com>
2024-07-18 19:14:29 +0300

    main file update

--- intermediate/55.1/main.py
+++ intermediate/55.1/main.py
@@ -1,1 +1,27 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+# ==================== Set up the Flight Search ====================
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
