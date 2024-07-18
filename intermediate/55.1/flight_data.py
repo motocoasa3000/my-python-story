61869e5257642783955dc58353e0aaf75c117df9
Lucian Cioba <luciancioba@gmail.com>
2024-07-18 19:14:05 +0300

    flight data file update

--- intermediate/55.1/flight_data.py
+++ intermediate/55.1/flight_data.py
@@ -1,3 +1,20 @@
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self, price, origin_airport, destination_airport, out_date, return_date, stops):
+        """
+        Constructor for initializing a new flight data instance with specific travel details.
+
+        Parameters:
+        - price: The cost of the flight.
+        - origin_airport: The IATA code for the flight's origin airport.
+        - destination_airport: The IATA code for the flight's destination airport.
+        - out_date: The departure date for the flight.
+        - return_date: The return date for the flight.
+        - stops: 0 for direct flights. 1 or more for indirect flights.
+        """
+        self.price = price
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
+        self.stops = stops
