b595bc99a9bc6795504f59694cbe4722e94595d7
Lucian Cioba <luciancioba@gmail.com>
2024-07-19 17:59:30 +0300

    data manager final

--- intermediate/55.1/data_manager.py
+++ intermediate/55.1/data_manager.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+
 class DataManager:
 
     def __init__(self):
-
         self._user = os.environ["SHEETY_USERNAME"]
         self._password = os.environ["SHEETY_PASSWORD"]
         # Save your Sheety endpoints an environment variables
@@ -29,3 +29,27 @@ class DataManager:
         # pprint(data)
         self.destination_data = data["prices"]
         return self.destination_data
+
+    # In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{self.prices_endpoint}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        response = requests.get(url=self.users_endpoint)
+        data = response.json()
+        # See how Sheet data is formatted so that you use the correct column name!
+        # pprint(data)
+        # Name of spreadsheet 'tab' with the customer emails should be "users".
+        self.customer_data = data["users"]
+        return self.customer_data
