05426f18c44fdb4d4f02babd0a2338fb0701d966
Lucian Cioba <luciancioba@gmail.com>
2024-08-21 13:56:35 +0300

    Data entry job automation project

---
+++ intermediate/68-data-entry-job-automation.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Part 1 - Scrape the links, addresses, and prices of the rental properties
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+# Use our Zillow-Clone website (instead of Zillow.com)
+response = requests.get("https://appbrewery.github.io/Zillow-Clone/", headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+# Create a list of all the links on the page using a CSS Selector
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
+# Python list comprehension (covered in Day 26)
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
+
+# Create a list of all the addresses on the page using a CSS Selector
+# Remove newlines \n, pipe symbols |, and whitespaces to clean up the address data
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
+
+# Create a list of all the prices on the page using a CSS Selector
+# Get a clean dollar price and strip off any "+" symbols and "per month" /mo abbreviation
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
