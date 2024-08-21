ce7e5719a560d956c0dd05cdcdaed8ad405ea8ad
Lucian Cioba <luciancioba@gmail.com>
2024-08-21 14:00:29 +0300

    Data entry job automation + selenium project

--- intermediate/68-data-entry-job-automation.py
+++ intermediate/68-data-entry-job-automation.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 # Part 1 - Scrape the links, addresses, and prices of the rental properties
 
@@ -34,3 +37,31 @@ all_price_elements = soup.select(".PropertyCardWrapper span")
 all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
 print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
 print(all_prices)
+
+# Part 2 - Fill in the Google Form using Selenium
+
+# Optional - Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    # Add fill in the link to your own Google From
+    driver.get("YOUR_GOOGLE_FORM_LINK_HERE")
+    time.sleep(2)
+
+    # Use the xpath to select the "short answer" fields in your Google Form.
+    # Note, your xpath might be different if you created a different form.
+    address = driver.find_element(by=By.XPATH,
+                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(by=By.XPATH,
+                               value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
