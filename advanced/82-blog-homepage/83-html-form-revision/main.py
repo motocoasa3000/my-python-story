21c8864959e6fbe6384daa5731a5ff685d2c6961
Lucian <luciancioba@gmail.com>
2024-10-09 17:10:53 +0300

    Make POST Requests with Flask and HTML forms

--- advanced/82-blog-homepage/83-html-form-revision/main.py
+++ advanced/82-blog-homepage/83-html-form-revision/main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    return "? Success! Form submitted"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
