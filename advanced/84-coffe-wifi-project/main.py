e06604780299feefb2fd901b0969b442b50425d8
Lucian Cioba <luciancioba@gmail.com>
2024-10-24 17:47:48 +0300

    building advanced forms with Flask-WTForms

--- advanced/84-coffe-wifi-project/main.py
+++ advanced/84-coffe-wifi-project/main.py
@@ -16,3 +16,22 @@ class CafeForm(FlaskForm):
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/add')
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        print("True")
+    return render_template('add.html', form=form)
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline="", encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html, cafes=list_of_rows')
+
+if __name__ == '__main__':
+    app.run(debug=True)
