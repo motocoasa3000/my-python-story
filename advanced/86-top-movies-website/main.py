3f9bf88e6df2acd6d50f4c5d44e8d3029352f75e
Lucian Cioba <luciancioba@gmail.com>
2024-11-07 13:58:14 +0200

    my top 10 movies website

--- advanced/86-top-movies-website/main.py
+++ advanced/86-top-movies-website/main.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired
 import requests
 
 
-MOVIE_DB_API_KEY = "USE_YOUR_OWN_CODE"
+MOVIE_DB_API_KEY = "2b6f88d0c7c00c89d4c7279584038562"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
