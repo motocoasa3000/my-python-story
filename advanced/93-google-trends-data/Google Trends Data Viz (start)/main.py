7078423e2a7d51b33b75d4e603d70528314b8b4b
Lucian Cioba <luciancioba@gmail.com>
2025-01-31 15:13:48 +0200

    Google trends data update

--- advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
+++ advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
@@ -70,3 +70,28 @@ df_btc_monthly = df_btc_price.resample('ME', on='DATE').last()
 
 print(df_btc_monthly.shape)
 df_btc_monthly.head()
+
+
+# Data Visualisation
+
+# Create locators for ticks on the time axis
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+years_fmt = mdates.DateFormatter('%Y')
+
+# Register date converters to avoid warning messages
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+
+# Tesla Stock Price vs Search Volume
+
+ax1 = plt.gca() # get current axis
+ax2 = ax1.twinx()
+
+ax1.set_ylabel('TSLA Stock Price', color=#E6232E)
+ax2.set_ylabel('Search Trend', color=#87CEEB)
+
+ax1.plot(df_tesla.MONTH, df_tesla.TSLA_USD_CLOSE, color=#E6232E)
+ax2.plot(df_tesla.MONTH, df_tesla.TSLA_WEB_SEARCH=color=#87CEEB)
+
