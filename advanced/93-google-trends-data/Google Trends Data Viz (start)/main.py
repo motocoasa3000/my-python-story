d4dc77309ea7b2cf01103aae7b59145e03d4841d
Lucian Cioba <luciancioba@gmail.com>
2025-02-02 16:47:10 +0200

    Google trends data update

--- advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
+++ advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
@@ -112,3 +112,30 @@ ax1.plot(df_tesla.MONTH, df_tesla.TSLA_USD_CLOSE, color='#E6232E', linewidth=3)
 ax2.plot(df_tesla.MONTH, df_tesla.TSLA_WEB_SEARCH, color='#87CEEB', linewidth=3)
 
 plt.show()
+
+
+# BTC price vs Search Volume
+plt.figure(figsize=(14, 8), dpi=120)
+plt.title('Bitcoin News Search vs Resampled Price', fontsize=18)
+plt.xticks(fontsize=14, rotation=45)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.set_ylabel('BTC Price', color='#F08F2E', fontsize=14)
+ax2.set_ylabel('Search Trend', color='Skyblue', fontsize=14)
+
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(years_fmt)
+ax1.xaxis.set_major_locator(months)
+
+ax1.set_ylim(bottom=0, top=15000)
+ax1.set_xlim([df_btc_monthly.index.min(), df_btc_monthly.index.max()])
+
+# Experiment with the linestyle and markers
+ax1.plot(df_btc_monthly.index, df_btc_monthly.CLOSE,
+         color='#F08F2E', linewidth=3, linestyle='--')
+ax2.plot(df_btc_monthly.index, df_btc_search.BTC_NEWS_SEARCH,
+         color='#87CEEB', linewidth=3, marker='o')
+
+plt.show()
