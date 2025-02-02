f169edd27b4e759114c608dffbe24c9c94f3204c
Lucian Cioba <luciancioba@gmail.com>
2025-02-03 00:34:23 +0200

    Google trends data update

--- advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
+++ advanced/93-google-trends-data/Google Trends Data Viz (start)/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
+from matplotlib.lines import lineStyles
 
 df_tesla = pd.read_csv('TESLA Search Trend vs Price.csv')
 
@@ -115,6 +115,7 @@ plt.show()
 
 
 # BTC price vs Search Volume
+
 plt.figure(figsize=(14, 8), dpi=120)
 plt.title('Bitcoin News Search vs Resampled Price', fontsize=18)
 plt.xticks(fontsize=14, rotation=45)
@@ -127,7 +128,7 @@ ax2.set_ylabel('Search Trend', color='Skyblue', fontsize=14)
 
 ax1.xaxis.set_major_locator(years)
 ax1.xaxis.set_major_formatter(years_fmt)
-ax1.xaxis.set_major_locator(months)
+ax1.xaxis.set_minor_locator(months)
 
 ax1.set_ylim(bottom=0, top=15000)
 ax1.set_xlim([df_btc_monthly.index.min(), df_btc_monthly.index.max()])
@@ -137,5 +138,37 @@ ax1.plot(df_btc_monthly.index, df_btc_monthly.CLOSE,
          color='#F08F2E', linewidth=3, linestyle='--')
 ax2.plot(df_btc_monthly.index, df_btc_search.BTC_NEWS_SEARCH,
          color='#87CEEB', linewidth=3, marker='o')
+
+plt.show()
+
+
+# Unemployment Benefits Search vs Actual Unemployment in the US
+
+plt.figure(figsize=(14, 8), dpi=120)
+plt.title('Monthly Search of "Unemployment Benefits" in the US vs the U/E rate', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(fontsize=14, rotation=45)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.set_ylabel('FRED U/E Rate', color='#A020F0', fontsize=14)
+ax2.set_ylabel('Search Trend', color='#87CEEB', fontsize=14)
+
+ax1.xaxis.set_major_locator(years)
+ax1.xaxis.set_major_formatter(years_fmt)
+ax1.xaxis.set_minor_locator(months)
+
+ax1.set_ylim(bottom=3, top=10.5)
+ax1.set_xlim([df_unemployment.MONTH.min(), df_unemployment.MONTH.max()])
+
+# Show the grid lines as dark grey lines
+ax1.grid(color='grey', linestyle='--')
+
+# Change the dataset used
+ax1.plot(df_unemployment.MONTH, df_unemployment.UNRATE,
+         '#A020F0', linewidth=3, linestyle='--')
+ax2.plot(df_unemployment.MONTH, df_unemployment.UE_BENEFITS_WEB_SEARCH,
+         '#87CEEB', linewidth=3)
 
 plt.show()
