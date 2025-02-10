377b183ec321ef5faaeac6494d0101599e8bbc46
Lucian Cioba <luciancioba@gmail.com>
2025-02-10 21:20:33 +0200

    Capstone project - predict house prices update nw

--- advanced/95-project-predict-house-prices/main.py
+++ advanced/95-project-predict-house-prices/main.py
@@ -13,4 +13,19 @@ pd.options.display.float_format = '{:,.2f'.format
 data = pd.read_csv('boston.csv', index_col=0)
 
 predicted_values = regr.predict(X_train)
+residuals =(y_train = predicted_values)
 
+plt.figure(dpi=150)
+plt.scatter(data.PRICE, np.log(data.PRICE))
+
+plt.title('Mapping the Original Price to a Log Price')
+plt.ylabel('Log Price')
+plt.xlabel('Actual $ Price in 000s')
+plt.show()
+
+# Starting Point: Average Values in the Dataset
+features = data.drop(['PRICE'], axis=1)
+average_vals = features.mean().values
+property_stats = pd.DataFrame(data=average_vals.reshape(1, len(features.columns)),
+                              columns=features.columns)
+property_stats
