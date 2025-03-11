987ed69711e09c3af6e6a18f23734b1086267118
Lucian Cioba <luciancioba@gmail.com>
2025-03-11 18:02:29 +0200

    Nobel prize analysis update: NaN values for birthdate are all organisations; NaN values for organisation_name

--- advanced/96-nobel-prize-analysis/main.py
+++ advanced/96-nobel-prize-analysis/main.py
@@ -5,12 +5,30 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-pd.options.display.flat_format = '{:,.2f'.format(
-    df_data = pd_read_csv('nobel_prize_data.csv')
-)
-df.data.shape
+pd.options.display.float_format = '{:,.2f}'.format
+
+df_data = pd.read_csv('nobel_prize_data.csv')
+df_data.shape
 df_data.head()
 df_data.tail()
 print(f'Any duplicates? {df_data.duplicated().values.any()}')
 print(f'Any NaN values among the data? {df_data.isna().values.any()}')
 df_data.isna().sum()
+
+# NaN values for birthdate are all organisations
+col_subset = ['year', 'category', 'laureate_type',
+              'birth_date', 'full_name', 'organization_name']
+df_data.loc[df_data.birth_date.isna()][col_subset]
+
+# NaN values for organisation_name
+col_subset = ['year', 'category', 'laureate_type', 'full_name', 'organization_name']
+df_data.loc[df_data.organization_name.isna()][col_subset]
+
+df_data.birth_date = pd.to_datetime(df_data.birth_date)
+
+separated_values = df_data.prize_share.str.split('/', expand=True)
+numerator = pd.to_numeric(separated_values[0])
+denomenator = pd.to_numeric(separated_values[1])
+df_data['share_pct'] = numerator / denomenator
+
+df_data.info()
