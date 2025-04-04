cd591a3d4c5232ae7bbc8517c22fe07c9f7f9366
Lucian Cioba <luciancioba@gmail.com>
2025-04-04 18:41:00 +0300

    Dr Semmelweis tragic discovery of handwashing update: Visualise the Total Number of Births and Deaths over Time

--- advanced/97-tragic-discovery-of-handwashing/main.py
+++ advanced/97-tragic-discovery-of-handwashing/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+
 
 # Notebook Presentation
 pd.options.display.float_format = '{:,.2f'.format
@@ -28,3 +30,48 @@ df_monthly
 # Check for Nan and Duplicates
 df_yearly.info()
 df_monthly.info()
+
+
+# Alternative to using .info()
+print(f'Any yearly NaN values? {df_monthly.isna().values.any()}')
+print(f'Any monthly NaN values? {df_yearly.isna().values.any()}')
+
+print(f'Any yearly duplicates? {df_yearly.duplicated().values.any()}')
+print(f'Any monthly duplicates? {df_monthly.duplicated().values.any()}')
+
+# Descriptive Statistics
+df_yearly.describe()
+df_monthly.describe()
+
+# Percentage pf Women Dying in childbirth
+prob = df_yearly.deaths.sum() / df_yearly.births.sum() * 100
+print(f'Chances of dying in the 1840s in Vienna: {prob:.3}%')
+
+
+# Visualise the Total Number of Births and Deaths over Time
+
+# Plot the Monthly data on twin axes
+plt.figure(figsize=(14,8), dpi=200)
+plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.grid(color='grey', linestyle='--')
+
+ax1.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.births,
+         color='crimson',
+         linewidth=2,
+         linestyle='--')
+plt.show()
