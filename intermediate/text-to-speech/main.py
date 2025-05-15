54a58c18afe74d6d6f649b1672ae27878bb200db
Lucian Cioba <luciancioba@gmail.com>
2025-05-15 16:21:16 +0300

    calculator tkinter

--- intermediate/text-to-speech/main.py
+++ intermediate/text-to-speech/main.py
@@ -1,8 +1,30 @@
 from gtts import gTTS
 import os
-text = open('text.txt', 'r').read()
+from tkinter import *
 
-language='en'
-output = gTTS(text=text, lang=language, slow=False)
-output.save('fileoutput.mp3')
-os.system("afplay fileoutput.mp3")
+root = Tk()
+canvas = Canvas(root,width=400,height=300)
+canvas.pack()
+
+def textToSpeech():
+    text = entry.get()
+    language='en'
+    output = gTTS(text=text, lang=language, slow=False)
+    output.save('fileoutput.mp3')
+    os.system("start fileoutput.mp3")
+
+entry = Entry(root)
+canvas.create_window(200,180,window=entry)
+
+button = Button(text="Speak", command=textToSpeech)
+canvas.create_window(200,230,window=button)
+
+
+root.mainloop()
+
+# text = open('text.txt', 'r').read()
+#
+# language='en'
+# output = gTTS(text=text, lang=language, slow=False)
+# output.save('fileoutput.mp3')
+# os.system("afplay fileoutput.mp3")
