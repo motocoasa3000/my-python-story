dcaeb6ad8984858c6876fd4048c5ccd79b6d70fe
Lucian Cioba <luciancioba@gmail.com>
2025-05-21 16:09:21 +0300

    calculator tkinter

--- intermediate/calculator-project/main.py
+++ intermediate/calculator-project/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import ast
+from click import command
 
 root =Tk()
 i = 0
@@ -16,6 +18,18 @@ def get_operation(operator):
 def clear_all():
     display.delete(0,END)
 
+def calculate():
+    entire_string = display.get()
+    try:
+        node = ast.parse(entire_string,mode='eval')
+        result = eval(compile(node, '<string>', 'eval'))
+        clear_all()
+        display.insert(0, result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Error")
+
+
 display = Entry(root)
 display.grid(row=1, columnspan=6)
 
@@ -24,11 +38,11 @@ counter = 0
 for x in range(3):
     for y in range(3):
         button_text = numbers[counter]
-        button = Button(root, text=button_text, width=2, height=2, command=lambda text=button_text:get_number(text))
+        button = Button(root, text=button_text, width=8, height=3, command=lambda text=button_text:get_number(text))
         button.grid(row=x+2, column=y)
         counter+=1
 
-button = Button(root,text="0", width=2, height=2, command=lambda :get_number(0))
+button = Button(root,text="0", width=8, height=3, command=lambda :get_number(0))
 button.grid(row=5, column=1)
 
 count = 0
@@ -36,13 +50,13 @@ operations = ['+', '-', '*', '/', '3.14', '%', '(',"**",")", "**2"]
 for x in range(4):
     for y in range(3):
         if count<len(operations):
-            button = Button(root, text=operations[count], width=2, height=2,
+            button = Button(root, text=operations[count], width=8, height=3,
                             command=lambda text=operations[count]:get_operation(text))
             count+=1
             button.grid(row=x + 2, column=y+3)
 
-Button(root,text="AC", width=2, height=2, command=clear_all).grid(row=5,column=0)
-Button(root,text="=", width=2, height=2).grid(row=5,column=2)
+Button(root,text="AC", width=8, height=3, command=clear_all).grid(row=5,column=0)
+Button(root,text="=", width=8, height=3,command=calculate).grid(row=5,column=2)
 
 
 root.mainloop()
