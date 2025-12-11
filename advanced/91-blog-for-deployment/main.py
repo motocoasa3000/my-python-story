31fe3e685cf314436a9643490fd1eb22db33c61e
Lucian Cioba <luciancioba@gmail.com>
2025-12-11 15:22:22 +0200

    old projects update

--- advanced/91-blog-for-deployment/main.py
+++ advanced/91-blog-for-deployment/main.py
@@ -1,6 +1,6 @@
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
-from flask_bootstrap import Bootstrap5
+# from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
@@ -18,7 +18,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
-Bootstrap5(app)
+# Bootstrap5(app)
 
 # Configure Flask-Login
 login_manager = LoginManager()
