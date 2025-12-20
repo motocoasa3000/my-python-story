4b9731b167bf712b86be97c5efd3181a091b38df
Lucian Cioba <luciancioba@gmail.com>
2025-12-20 23:21:59 +0200

    old projects update

--- advanced/84-coffe-wifi-project/main.py
+++ advanced/84-coffe-wifi-project/main.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
+from flask_bootstrap5 import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
